d00262cae35eab6cc3f418fd3d62920ce4cb885b
zackkrida <zackwcote@gmail.com>
2018-05-10 12:51:33 -0400

    only uglify on production

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import VuePlugin from 'rollup-plugin-vue'
 // rollup.config.js
 export default {
   input: 'src/main.js',
-  plugins: [VuePlugin(), uglify()],
+  plugins: [VuePlugin(), process.env.NODE_ENV === 'production' && uglify()],
   output: {
     file: 'dist/vue-simple-maps.js',
     format: 'cjs'
